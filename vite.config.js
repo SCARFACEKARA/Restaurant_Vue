9a68a81880809148fc2d66a0079313760f868aa6
SCARFACEKARA <rantonirinaanthony@gmail.com>
2025-02-18 11:50:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0',  // Permet d'accéder via une adresse IP locale
-    port: 5173,       // Choisis un port (par défaut 5173)
-    strictPort: true, // Empêche le port de changer si 5173 est occupé
+    port: process.env.PORT || 3000,  // Définit le port sur 3000 pour Railway
+    host: "0.0.0.0",
+    strictPort: true   // Empêche Vite de changer le port
+  },
+  preview: {
+    port: process.env.PORT || 3000,  // Port pour `vite preview`
+    host: "0.0.0.0",
+    allowedHosts: ["restaurantvue-production.up.railway.app"]
   }
-})
+});
